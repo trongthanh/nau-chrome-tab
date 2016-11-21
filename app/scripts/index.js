d3a5289f5b75ae9876ed0176525e9755fb7bd0e3
Thanh Tran <trongthanh@gmail.com>
2016-11-21 18:43:14 +0700

    Implement editing and storing greeting name

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -71,4 +71,54 @@
 	}
 
 	$('#greeting-text').textContent = greeting;
+
+	// name edit
+	let nameComp = $('#greeting-name');
+	let nameInput = nameComp.querySelector('.greeting__input');
+	let nameText = nameComp.querySelector('.greeting__name');
+	let currentName = localStorage.getItem('greetingName');
+
+	if (!currentName) {
+		localStorage.setItem('greetingName', 'set me');
+	}
+	// set name at startup
+	nameText.textContent = currentName;
+
+	nameComp._.events({
+		click(event) {
+			event.stopPropagation();
+			nameComp.classList.add('greeting__name-comp--active');
+			nameInput.focus();
+			nameInput.setSelectionRange(0, nameInput.value.length);
+			// bind click outside
+			document.addEventListener('click', nameInputSubmit);
+		},
+	});
+
+	nameInput._.events({
+		blur() {
+			nameInputSubmit();
+		},
+		keypress(event) {
+			// enter
+			if (event.charCode === 13) {
+				nameInputSubmit();
+			}
+		}
+	});
+
+	function nameInputSubmit(event) {
+		let newName = nameInput.value.trim();
+
+		if (newName) {
+			nameText.textContent = newName + '.';
+			localStorage.setItem('greetingName', newName);
+		} else {
+			nameText.textContent = localStorage.getItem('greetingName');
+		}
+
+		nameComp.classList.remove('greeting__name-comp--active');
+		document.removeEventListener('click', nameInputSubmit);
+	}
+
 }());
