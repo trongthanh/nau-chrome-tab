3e03201effa5b3cf1a45e05316d56af176055ea9
Thanh Tran <trongthanh@gmail.com>
2017-12-10 14:37:11 +0700

    Rewrite layout using CSS grid;

--- app/scripts/wallpaper.js
+++ app/scripts/wallpaper.js
@@ -10,7 +10,7 @@ const defaultPhoto = {
 	imgUrl: 'https://images.unsplash.com/photo-1462688681110-15bc88b1497c?dpr=1&auto=compress,format&w=1920&q=80&cs=tinysrgb',
 	imgId: 'gzeUpbjoTUA',
 	authorName: 'Hoach Le Dinh',
-	authorUsername: '@hoachld',
+	authorUsername: 'hoachld',
 };
 
 const Wallpaper = {
