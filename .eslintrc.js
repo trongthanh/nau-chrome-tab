329c0a4d24119525d8ce38912b50811571f2391c
Thanh Tran <trongthanh@gmail.com>
2017-12-10 15:34:13 +0700

    Configure new Vue app from vue-cli, make sure it start-able

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,9 @@
 // Nau standard eslint rules, save it as .eslintrc.js
 /* eslint-disable quote-props */
 module.exports = {
-	'root': true,
-	'extends': [
-		'nau',
-	],
-	'rules': {
+	root: true,
+	extends: ['nau'],
+	rules: {
 		'prefer-template': 'off',
 		'spaced-comment': 'off',
 		'no-underscore-dangle': 'off',
@@ -13,42 +11,60 @@ module.exports = {
 		'prefer-spread': 'off',
 		'max-len': 'off',
 		'vars-on-top': 'off',
-		'strict': 'off',
+		strict: 'off',
 		'prefer-arrow-callback': 'off',
 		'no-alert': 'off',
 		'global-require': 'off',
+		// don't require .vue extension when importing
+		'import/extensions': [
+			'error',
+			'always',
+			{
+				js: 'never',
+				vue: 'never',
+			},
+		],
+		// allow optionalDependencies
+		'import/no-extraneous-dependencies': [
+			'error',
+			{
+				optionalDependencies: ['test/unit/index.js'],
+			},
+		],
+		'import/no-anonymous-default-export': 'off',
 	},
-	'globals': {
-		'browser': false,
+	globals: {
+		browser: false,
 		// bliss globals
-		'$': false,
-		'$$': false,
+		$: false,
+		$$: false,
 	},
-	'env': {
-		'browser': true,
-		'node': true,
-		'es6': true,
-		'mocha': true,
+	env: {
+		browser: true,
+		node: true,
+		es6: true,
+		mocha: true,
 	},
-	// 'parser': 'babel-eslint',
-	'parserOptions': {
-		'ecmaVersion': 6,
-		// 'sourceType': 'module',
-		'ecmaFeatures': {
-			'impliedStrict': false,
+	parser: 'babel-eslint',
+	parserOptions: {
+		ecmaVersion: 6,
+		sourceType: 'module',
+		ecmaFeatures: {
+			impliedStrict: false,
 			// 'jsx': true,
 			// 'classes': true,
 		},
 	},
-	'plugins': [
+	plugins: [
 		'import',
+		'html',
 		// react (install eslint-plugin-react)
 		// babel (install eslint-plugin-babel)
 	],
-	'settings': {
+	settings: {
 		'import/resolver': {
-			'webpack': {
-				'config': 'webpack.config.js',
+			webpack: {
+				config: 'build/webpack.base.conf.js',
 			},
 		},
 	},
---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+	<div id="app">
+		<img src="/images/icon-128.png" />
+		<HelloWorld/>
+	</div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld';
+
+const vueApp = {
+	name: 'app',
+	components: {
+		HelloWorld,
+	},
+};
+
+export default vueApp;
+</script>
+
+<style>
+#app {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
+	margin-top: 60px;
+}
+</style>
