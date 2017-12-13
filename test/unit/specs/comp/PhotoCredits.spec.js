81b01d2c3356c1b0635d69faaca4ab17ce961d1b
Thanh Tran <trongthanh@gmail.com>
2017-12-13 22:22:43 +0700

    Write proper unit test for PhotoCredit

--- test/unit/specs/comp/PhotoCredits.spec.js
+++ test/unit/specs/comp/PhotoCredits.spec.js
@@ -3,9 +3,23 @@ import PhotoCredit from '@/components/PhotoCredit';
 
 describe('PhotoCredit.vue', () => {
 	it('should render correct contents', () => {
-		const Constructor = Vue.extend(PhotoCredit);
-		const vm = new Constructor().$mount();
-		// expect(vm.$el.querySelector('.hello h1').textContent).toEqual('Welcome to Your Vue.js App');
-		expect('Welcome to Your Vue.js App').toEqual('Welcome to Your Vue.js App');
+		const imgData = {
+			imgUrl: 'https://images.unsplash.com/photo-1',
+			imgId: 'unsplashimgid',
+			authorName: 'Author Name',
+			authorUsername: 'authorUsername',
+		};
+
+		const PhotoCreditComp = Vue.extend(PhotoCredit);
+		const vm = new PhotoCreditComp({
+			propsData: { imgData },
+		}).$mount();
+
+		expect(vm.$el.querySelector('.photo-credit__author').textContent).toEqual(imgData.authorName);
+		expect(vm.$el.querySelector('.photo-credit__author').href).toEqual(
+			`https://unsplash.com/@${imgData.authorUsername}`,
+		);
+		expect(vm.$el.querySelector('.photo-credit__origin').textContent).toEqual('Unsplash');
+		expect(vm.$el.querySelector('.photo-credit__origin').href).toEqual(`https://unsplash.com/photos/${imgData.imgId}`);
 	});
 });
