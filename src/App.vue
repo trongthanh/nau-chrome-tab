5537222a50a4c0bdc2a8c6997ebb6f8f37a1242c
Thanh Tran <thanh.trantrong@lazada.com>
2019-05-02 18:06:53 +0700

    Update bottom right logo

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 				<Quicklinks />
 			</div>
 			<div class="main__item main__item--top-right">
-				<div>Weather info</div>
+				<!-- <div>Weather info</div> -->
 			</div>
 			<div class="main__item main__item--center">
 				<Clock />
@@ -19,8 +19,8 @@
 			</div>
 			<div class="main__item main__item--bottom-right">
 				<div class="brand-logo">
-					<a href="https://int3ractive.com" class="quick-links__link" title="Int3ractive">
-						<i class="mdi mdi--power-plug" />
+					<a href="https://int3ractive.com" class="quick-links__link logo" title="Made in Saigon with">
+						<i class="mdi mdi--heart" />
 					</a>
 				</div>
 			</div>
